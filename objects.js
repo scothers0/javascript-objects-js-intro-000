79c0fc590fedd3baba8e200fa5231aadd5a31c63
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-10 18:03:49 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {artistName: "dylan", 
+                track: "like a rolling stone"
+              };
+
+function updatePlaylist(playlist, name, song){
+      playlist[name]
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, name) {
+  
+}
